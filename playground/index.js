846075af4fcc3bf5424dbfd197b7423b60173d2c
Irene Fuente Romero <ifuenteromero@gmail.com>
2025-10-01 11:06:57 +0200

    Refactor model selection criteria and add package configuration files

--- playground/index.js
+++ playground/index.js
@@ -1,21 +1,19 @@
-import { get_encoding } from "tiktoken";
+import OpenAi from "openai";
 
-const encoding = get_encoding("cl100k_base");
-// cl100k_base is an encoding. An encoding is a dictionary that maps token IDs to tokens. Ej: 904 => "hello"
-// cl => chat language 100k => 100,000 unique tokens
+const client = new OpenAi({
+  apiKey: OPENAI_API_KEY,
+});
 
-const tokens = encoding.encode(
-  "Hello, world!. This is the first time I use tiktoken."
-);
+const stream = await client.responses.create({
+  model: "gpt-4.1",
+  input: "Write a story about a robot",
+  temperature: 0.7,
+  max_output_tokens: 50,
+  stream: true,
+});
 
-console.log({ tokens });
-// {
-//   tokens: Uint32Array(14) [
-//     9906,  11, 1917, 15725,
-//     1115, 374,  279,  1176,
-//      892, 358, 1005, 87272,
-//     5963,  13
-//   ]
-// }
-// Each number is a token ID that maps to a token
-// So when working with large amount of text, we can use the tick - token library to count tokens before sending a prompt to a LLM
+// Async iterable
+
+for await (const event of stream) {
+  if (event.delta) process.stdout.write(event.delta);
+}
